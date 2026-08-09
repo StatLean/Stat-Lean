22586dec2fed3e80a9f2a456c0c9021b7068d083
Junwei Lu <junweilu@hsph.harvard.edu>
2026-08-09 22:20:03 +0900

    umbrella: import IFSMinorization (laptop)

--- StatLean/TimeSeries.lean
+++ StatLean/TimeSeries.lean
@@ -6,6 +6,7 @@ import StatLean.TimeSeries.ForMathlib.Markov.Chain
 import StatLean.TimeSeries.ForMathlib.Markov.GeometricErgodicity
 import StatLean.TimeSeries.ForMathlib.Markov.HarrisTheorem
 import StatLean.TimeSeries.ForMathlib.Ergodic.Birkhoff
+import StatLean.TimeSeries.ForMathlib.Markov.IFSMinorization
 import StatLean.TimeSeries.ForMathlib.Markov.TwoSidedChain
 import StatLean.TimeSeries.ForMathlib.Probability.TriangularCLT
 import StatLean.TimeSeries.ForMathlib.Probability.MartingaleCLT.Defs
