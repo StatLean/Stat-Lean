74fba37d0bf5eb278295a1b62ded86cc56eaa744
Junwei Lu <junweilu@hsph.harvard.edu>
2026-08-10 05:17:43 +0900

    remove ts/s13 lane's stray axiom-check scratch

--- scratch/AxCheck.lean
+++
@@ -1,4 +0,0 @@
-import StatLean.TimeSeries.Stationarity.ARCH
-#print axioms StatLean.TimeSeries.archInf_mds
-#print axioms StatLean.TimeSeries.archInf_condexp_strict_past
-#print axioms StatLean.TimeSeries.archInfDrift_ae_eq
